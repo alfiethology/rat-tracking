a8ccd318ec04906886becc0ff3f6e919628bfa1f
Alfie <or22503@bristol.ac.uk>
2025-04-04 10:40:01 +0100

    added rat_monitor_settings

--- rat_monitor.py
+++ rat_monitor.py
@@ -6,15 +6,7 @@ from tqdm import tqdm
 from ultralytics import YOLO
 import time
 import datetime
-
-# ====== Config ======
-MODEL_PATH = "/home/or22503/Louise_rat_tracking/runs/detect/train2/weights/best.pt"
-VIDEO_FOLDER = "/home/or22503/Louise_rat_tracking/videos/raw_videos"
-AREAS_JSON_PATH = "/home/or22503/Louise_rat_tracking/areas.json"
-SCHEDULE_JSON_PATH = "/home/or22503/Louise_rat_tracking/multi_video_time_stamps.json"
-CSV_OUTPUT_FOLDER = "/home/or22503/Louise_rat_tracking/csv_outputs"
-MIN_CONFIDENCE = 0.8
-FRAME_SKIP = int(input("frame skip: "))  # Set this to 1 for every frame, 5 for every 5th frame, etc.
+from rat_monitor_settings import *
 
 # ====== Start Timer & date ======
 date = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
---
+++ rat_monitor_settings.py
@@ -0,0 +1,7 @@
+MODEL_PATH = "/home/or22503/Louise_rat_tracking/runs/detect/train2/weights/best.pt"
+VIDEO_FOLDER = "/home/or22503/Louise_rat_tracking/videos/raw_videos"
+AREAS_JSON_PATH = "/home/or22503/Louise_rat_tracking/areas.json"
+SCHEDULE_JSON_PATH = "/home/or22503/Louise_rat_tracking/multi_video_time_stamps.json"
+CSV_OUTPUT_FOLDER = "/home/or22503/Louise_rat_tracking/csv_outputs"
+MIN_CONFIDENCE = 0.8
+FRAME_SKIP = 1  # Set this to 1 for every frame, 5 for every 5th frame, etc.
